5be9363e91ae5259a5d7aaa37d61a29b1aacfa9f
Edosa Kelvin <edosakelvin@gmail.com>
2017-11-12 10:55:21 +0100

    Added ParserFor attribute
    This attribute will be used to decorate classes which will serve as page parsers

---
+++ Netstats/Network/ParserForAtrribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netstats.Network
+{
+    public class ParserForAtrribute : Attribute
+    {
+        PageType For { get; }
+
+        public ParserForAtrribute(PageType pageType)
+        {
+            For = pageType;
+        }
+    }
+}
